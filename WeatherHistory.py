d11859181954f1aef559f63d15bb82ab830e51d7
Alexander Bangert <ab@xentary.de>
2014-06-12 21:56:30 +0200

    Prototyp

---
+++ WeatherHistory.py
@@ -0,0 +1,33 @@
+__author__ = 'ab'
+
+import simplejson
+import urllib
+import urllib2
+import xml.sax
+
+class WeatherImportHandler(xml.sax.ContentHandler):
+    temperature = None
+    def startElement(self, name, attrs):
+        if self.temperature is not None:         # Wir wollen nur das erste Element
+            return
+        if name == "temperature":
+            self.temperature = attrs['value']
+
+address = urllib.urlencode({"q": "51427, Bergisch Gladbach, Germany"})
+url = "http://nominatim.openstreetmap.org/search.php?%s&format=json&addressdetails=0" % address
+req = urllib2.Request(url)
+opener = urllib2.build_opener()
+input = opener.open(req)
+
+data = simplejson.load(input)
+print data[0]["lat"] + " " + data[0]["lon"]
+
+
+url = "http://api.yr.no/weatherapi/locationforecast/1.9/?lat=%s;lon=%s" % (data[0]["lat"], data[0]["lon"])
+req = urllib2.Request(url)
+input = opener.open(req)
+xmldata = input.read()
+
+handler = WeatherImportHandler()
+xml.sax.parseString(xmldata, handler)
+print handler.temperature
